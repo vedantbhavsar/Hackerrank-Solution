b7054b4b008445b101fd8342aac2f477d882c42e
Vedant <37434481+vedantbhavsar@users.noreply.github.com>
2020-04-12 15:10:37 +0530

    Create Cavity-Map.cpp

---
+++ problem-solving/Cavity-Map.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+#include <string>
+
+using namespace std;
+
+#define CONDITION_1 grid[i][j] > grid[i-1][j] && grid[i][j] > grid[i][j-1]
+#define CONDITION_2 grid[i][j] > grid[i][j+1] && grid[i][j] > grid[i+1][j]
+// Complete the cavityMap function below.
+vector<string> cavityMap(vector<string> grid) {
+
+    for(int i=1; i<grid.size()-1; i++){
+        for(int j=1; j<grid.size()-1; j++){
+            if(CONDITION_1 && CONDITION_2)
+                grid[i][j] = 'X';
+        }
+    }
+    return grid;
+
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    vector<string> grid(n);
+
+    for (int i = 0; i < n; i++) {
+        string grid_item;
+        getline(cin, grid_item);
+
+        grid[i] = grid_item;
+    }
+
+    vector<string> result = cavityMap(grid);
+
+    for (int i = 0; i < result.size(); i++) {
+        fout << result[i];
+
+        if (i != result.size() - 1) {
+            fout << "\n";
+        }
+    }
+
+    fout << "\n";
+
+    fout.close();
+
+    return 0;
+}
