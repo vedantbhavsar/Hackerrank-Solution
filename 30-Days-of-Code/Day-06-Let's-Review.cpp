b2d6af445e3902dd4c3b983da69b6f078c212da2
Vedant <37434481+vedantbhavsar@users.noreply.github.com>
2020-06-15 19:10:50 +0530

    Update Day-06-Let's-Review.cpp

--- 30-Days-of-Code/Day-06-Let's-Review.cpp
+++ 30-Days-of-Code/Day-06-Let's-Review.cpp
@@ -7,35 +7,35 @@ using namespace std;
 
 
 int main() {
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
-    int T;
-    cin >> T;
-    
-    for (size_t t = 0 ; t < T ; ++t) {
-        string S;
-        cin >> S;
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
+    int N;
+    cin >> N;   //No. of test cases
 
-        bool funny = true;
-        char f_last, b_last;
-        auto f = S.begin();
-        auto b = S.rbegin();
-        f_last = *f;
-        b_last = *b;
-        for (++f, ++b ; f != S.end() ; ++f, ++b) {
-            //cout << "[f|f_l|b|b_l]" << endl << "[" << *f << "|" << f_last << "|" << *b << "|" << b_last << "]" << endl;
-            int f_diff = abs(tolower(*f) - tolower(f_last)), b_diff = abs(tolower(*b) - tolower(b_last));
-            if (f_diff != b_diff) {
-                funny = false;
-                break;
+    for (int i = 0; i < N; i++) 
+    {
+        string str;
+        cin >> str;
+
+        for (int j = 0; j < str.length(); j++) 
+        {
+            if (j % 2 == 0)
+            {
+                cout << str[j];
             }
-            f_last = *f;
-            b_last = *b;
         }
-        if ( funny ) {
-            cout << "Funny" << endl;
-        } else {
-            cout << "Not Funny" << endl;
+
+        cout << " ";
+
+        for (int j = 0; j < str.length(); j++) 
+        {
+            if (j % 2 != 0)
+            {
+                cout << str[j];
+            }
         }
+
+        cout << endl;
     }
+
     return 0;
 }
