903007639b84c840d415735ce1a206e51ca32dfb
Vedant <37434481+vedantbhavsar@users.noreply.github.com>
2020-06-15 19:24:47 +0530

    Update Day-10-Binary-Numbers.cpp

--- 30-Days-of-Code/Day-10-Binary-Numbers.cpp
+++ 30-Days-of-Code/Day-10-Binary-Numbers.cpp
@@ -1,42 +1,23 @@
-#include <map>
-#include <set>
-#include <list>
-#include <cmath>
-#include <ctime>
-#include <deque>
-#include <queue>
-#include <stack>
-#include <string>
-#include <bitset>
-#include <cstdio>
-#include <limits>
-#include <vector>
-#include <climits>
-#include <cstring>
-#include <cstdlib>
-#include <fstream>
-#include <numeric>
-#include <sstream>
-#include <iostream>
-#include <algorithm>
-#include <unordered_map>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-
-int main(){
-    int n, max1s = 0, temp1s = 0;
+int main()
+{
+    int n, count = 0, max = 0;
     cin >> n;
-    bitset<20> bits(n);
-    
-    for(size_t bit = 0; bit < bits.size() ; ++bit) {
-        if ( bits[bit] ) {
-            ++temp1s;
-        } else {
-            if ( temp1s > max1s ) { max1s = temp1s; }
-            temp1s = 0;
-        }
+
+    while(n)
+    {
+        if (n&1)
+            count++;
+        else 
+            count = 0;
+        if (max < count)
+            max = count;
+        n>>=1;
     }
-    cout << max1s;
+    cout << max;
+
     return 0;
 }
