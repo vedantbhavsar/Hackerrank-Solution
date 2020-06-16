3ba204b9c76302d53f1eeea96fd54c13b4c769a3
Vedant <37434481+vedantbhavsar@users.noreply.github.com>
2020-06-16 12:36:28 +0530

    Create Basic-Data-Types.cpp

---
+++ C++/Introduction/Basic-Data-Types.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    // Complete the code.
+    int a;
+    long c;
+    char e;
+    float b;
+    double d;
+
+    cin >> a >> c >> e >> b >> d;
+
+    //cout << a << " " << c << " " << e << " " << b << " " << d << endl;
+    printf("%d \n%ld \n%c \n%f \n%lf", a, c, e, b, d);
+
+    return 0;
+}
