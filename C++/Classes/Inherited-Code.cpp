83f475c275ab7fef72109a47e063f1bf9a73d210
Vedant <37434481+vedantbhavsar@users.noreply.github.com>
2020-06-16 12:57:34 +0530

    Create Inherited-Code.cpp

---
+++ C++/Classes/Inherited-Code.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <exception>
+using namespace std;
+
+/* Define the exception here */
+struct BadLengthException : exception{
+  
+    string s;
+    BadLengthException(int n) : s(to_string(n)){}
+    const char *what() const noexcept override{
+        return s.c_str();
+    }
+
+};
+
