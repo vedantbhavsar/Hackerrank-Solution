9f4c0e6e899ca22974a0c72e4fff1a12661a6c77
Vedant <37434481+vedantbhavsar@users.noreply.github.com>
2020-06-16 12:58:49 +0530

    Create Exceptional-Server.cpp

---
+++ C++/Classes/Exceptional-Server.cpp
@@ -0,0 +1,16 @@
+{
+		/* Enter your code here. */
+        try{
+            cout << Server::compute(A,B) << endl;
+        }
+        catch(bad_alloc& error){
+            cout << "Not enough memory" << endl;
+        }
+        catch(exception& error){
+            cout << "Exception: " << error.what() << endl;
+        }
+        catch(...){
+            cout << "Other Exception" << endl;
+        }
+
+	}
