e21e56bf7611b9752aeebeeccdad8c5946c94ab3
Vedant <37434481+vedantbhavsar@users.noreply.github.com>
2020-06-21 14:10:17 +0530

    Update Preprocessor-Solution.cpp

--- C++/Other-Concepts/Preprocessor-Solution.cpp
+++ C++/Other-Concepts/Preprocessor-Solution.cpp
@@ -3,7 +3,7 @@
 #define io(v) cin >> v
 #define INF 100000000
 #define foreach(v, i) for(int i = 0; i < v.size(); i++)
-#define FUNCTION(name, op) void name(int& a, int b) { if (b op a) a = b; }
+#define FUNCTION(name, op) void name(int& a, int& b) { if (b op a) a = b; }
 
 #include <iostream>
 #include <vector>
