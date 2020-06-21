525f2ddec1863a00b7af4392737759b95425ee97
Vedant <37434481+vedantbhavsar@users.noreply.github.com>
2020-06-21 13:10:18 +0530

    Update Day-27-Testing.cpp

--- 30-Days-of-Code/Day-27-Testing.cpp
+++ 30-Days-of-Code/Day-27-Testing.cpp
@@ -1,20 +1,62 @@
+#include <algorithm>
 #include <iostream>
+#include <stdexcept>
+#include <vector>
+#include <cassert>
+#include <set>
 
-int main () {
-    std::cout << "5" << std::endl;
+using namespace std;
 
-    std::cout << "7 5" << std::endl;
-    std::cout << "0 0 -3 -7 1 4 8" << std::endl;
-
-    std::cout << "3 2" << std::endl;
-    std::cout << "-1 2 0" << std::endl;
-
-    std::cout << "5 4" << std::endl;
-    std::cout << "0 2 -3 -1 8" << std::endl;
-
-    std::cout << "4 2" << std::endl;
-    std::cout << "0 2 -3 1" << std::endl;
-
-    std::cout << "6 5" << std::endl;
-    std::cout << "0 0 -3 1 2 -1" << std::endl;
+int minimum_index(vector<int> seq) {
+    if (seq.empty()) {
+        throw invalid_argument("Cannot get the minimum value index from an empty sequence");
+    }
+    int min_idx = 0;
+    for (int i = 1; i < seq.size(); ++i) {
+        if (seq[i] < seq[min_idx]) {
+            min_idx = i;
+        }
+    }
+    return min_idx;
 }
+
+class TestDataEmptyArray {
+public:
+    static vector<int> get_array() {
+        // complete this function
+        vector<int> v;
+        return v;
+    }
+
+};
+
+class TestDataUniqueValues {
+public:
+    static vector<int> get_array() {
+        // complete this function
+        vector<int> v = {1,2};
+        return v;
+    }
+
+    static int get_expected_result() {
+        // complete this function
+        return 0;
+    }
+
+};
+
+class TestDataExactlyTwoDifferentMinimums {
+public:
+    static vector<int> get_array() {
+        // complete this function
+        vector<int> v = {1,1};
+        return v;
+    }
+
+    static int get_expected_result() {
+        // complete this function
+        return 0;
+    }
+
+};
+
