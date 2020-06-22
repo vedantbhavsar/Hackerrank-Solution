5ed01522b45298abded6a3e2e449a4545c7b5c56
Vedant <37434481+vedantbhavsar@users.noreply.github.com>
2020-06-22 21:40:13 +0530

    Create Day-5-Arrow-Functions.js

---
+++ 10-Days-of-Javascript/Day-5-Arrow-Functions.js
@@ -0,0 +1,19 @@
+/*
+ * Modify and return the array so that all even elements are doubled and all odd elements are tripled.
+ * 
+ * Parameter(s):
+ * nums: An array of numbers.
+ */
+
+function modifyArray(nums) {
+
+    return nums.map( n => n = (n % 2 == 0) ? n * 2: n * 3);
+    
+}
+
+function main() {
+    const n = +(readLine());
+    const a = readLine().split(' ').map(Number);
+    
+    console.log(modifyArray(a).toString().split(',').join(' '));
+}
