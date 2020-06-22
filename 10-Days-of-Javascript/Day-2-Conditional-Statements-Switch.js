4fc500b678656c83ecc3458f05f5d49457229f97
Vedant <37434481+vedantbhavsar@users.noreply.github.com>
2020-06-22 15:55:07 +0530

    Create Day-2-Conditional-Statements-Switch.js

---
+++ 10-Days-of-Javascript/Day-2-Conditional-Statements-Switch.js
@@ -0,0 +1,46 @@
+function getLetter(s) {
+
+    let letter;
+    
+    // Write your code here
+    var word = s[0];
+
+    switch(word){
+
+        case 'a':
+        case 'e':
+        case 'i':
+        case 'o':
+        case 'u':
+            letter = 'A';
+            break;
+
+        case 'b':
+        case 'c':
+        case 'd':
+        case 'f':
+        case 'g':
+            letter = 'B';
+            break;
+
+        case 'h':
+        case 'j':
+        case 'k':
+        case 'l':
+        case 'm':
+            letter = 'C';
+            break;
+
+        default:
+            letter = 'D';
+            
+    }
+    return letter;
+
+}
+
+function main() {
+    const s = readLine();
+    
+    console.log(getLetter(s));
+}
