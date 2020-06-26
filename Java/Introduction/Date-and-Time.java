1dc8812270b7148c6109804acfef8e67cb37b5d7
Vedant <37434481+vedantbhavsar@users.noreply.github.com>
2020-06-26 12:26:22 +0530

    Create Date-and-Time.java

---
+++ Java/Introduction/Date-and-Time.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Result {
+
+    /*
+     * Complete the 'findDay' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts following parameters:
+     *  1. INTEGER month
+     *  2. INTEGER day
+     *  3. INTEGER year
+     */
+
+    public static String findDay(int month, int day, int year) {
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(year, month-1, day);
+
+        return (new DateFormatSymbols().getWeekdays()[calendar.get(Calendar.DAY_OF_WEEK)].toUpperCase());
+
+    }
+
+}
+
+public class Solution {
