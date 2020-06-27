97a4cca0ddc432a82adc51074236f4456cbb73f7
Vedant <37434481+vedantbhavsar@users.noreply.github.com>
2020-06-27 18:41:56 +0530

    Added one more way to solve

--- Java/Strings/String-Reverse.java
+++ Java/Strings/String-Reverse.java
@@ -7,7 +7,22 @@ public class Solution {
         
         Scanner sc=new Scanner(System.in);
         String A=sc.next();
+        
+        //Solution 1
+        /* Enter your code here. Print output to STDOUT. */
+        char arr[] = A.toCharArray();
+        String rev = "";
 
+        for(int i=arr.length-1; i>=0; i--){
+            rev += arr[i];
+        }
+
+        if(A.equals(rev))
+            System.out.println("Yes");
+        else
+            System.out.println("No");
+        
+        //Solution 2
         /* Enter your code here. Print output to STDOUT. */
         String rev = new StringBuilder(A).reverse().toString();
         
