c5f7f82ea045b7258ac24442dedc498dc2f9a923
Vedant <37434481+vedantbhavsar@users.noreply.github.com>
2021-02-17 19:54:20 +0530

    Create Pangrams.java

---
+++ Strings/Pangrams.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the pangrams function below.
+    static String pangrams(String s) {
+        String st = s.toLowerCase();
+        for(char c='a'; c<='z'; c++){
+            if(st.indexOf(c) < 0)
+                return "not pangram";
+        }
+        return "pangram";
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s = scanner.nextLine();
+
+        String result = pangrams(s);
+
+        bufferedWriter.write(result);
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
